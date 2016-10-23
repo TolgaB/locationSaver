ae8cf6ee4ab0ab9d3e178d8aefc8a165f6a9b49f
kevinfang2 <kevkev0123@gmail.com>
2016-10-23 06:49:11 -0700

    like can you push like m8 can you push can you push

--- locationSaver/locationSaver/ProfileViewController.m
+++ locationSaver/locationSaver/ProfileViewController.m
@@ -83,7 +83,7 @@
 }
 
 - (BOOL) getCredit {
-    NSString *path = @"https://syf2020.syfwebservices.com/syf/applyForCredits";
+    NSString *path = @"https://syf2020.syfwebservices.com/syf/applyForCredit";
     
     NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:path]];
     [request setHTTPMethod:@"POST"];
@@ -93,10 +93,10 @@
     [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     
     NSString *firstName = @"same";
-    NSString *lastName = @"Approved";
-    NSString *whateverthelastthingwas = @"credit";
+    NSString *lastName = @"APPROVAL";
+    NSString *whateverthelastthingwas = @"10";
     
-    NSString *str = [NSString stringWithFormat:@"{\"apply\" : {\"applyRequest\" : {\"authCosApplicantData\" : {\"lastNameapp\" : %@,\"firstNameapp\" : %@,\"middleInitialapp\" : \"\",\"suffixapp\" : \"\",\"ssn1\" : \"\",\"birthDateapp\" : \"\",\"driverLicenseNumberapp\" : \"\",\"driverLicenseStateapp\" : \"\",\"signatureIndicatorapp\" : \"\",\"titleapp\" : \"\",\"occupationCodeapp\" : "",\"incomeAnnualapp\" : \"\",\"address\" : {\"address2app\" : "",\"stateapp\" : \"\",\"address1app\" : \"\",\"cityapp\" : \"\",\"zipCodeapp\" : \"\"},\"membershipNumberapp\" : \"\",\"ssnOverrideapp\" : \"\",\"driverLicenseExpiryDateapp\" : \"\"},\"spouseDetails\" : {\"spFirstName\" : \"\",\"spMiddleName\" : \"\",\"spLastName\" : \"\",\"address\" : {\"address2spd\" : \"\",\"statespd\" : \"\",\"address1spd\" : \"\",\"cityspd\" : \"\",\"zipCodespd\" : \"\"}},\"merchantData\" : {\"clientDepartment\" : \"\",\"iovationRequestType\" : \"application\",\"operator\" : \"ECOM\",\"clientData\" : \"\",\"merchantNumber\" : \"0000000000010032\",\"country\" : \"US\",\"deviceType\" : \"T\",\"originalProductCode\" : \"010\",\"rewardsCode\" : \"\",\"membershipNumber\" : \"\",\"primaryGroup\" : \"000\",\"empAccountCode\" : \"Y\",\"salesPerson\" : \"213006748\",\"clientDataShare\" : \"\",\"sendEmail\" : \"\",\"referalCode\" : \"\",\"register\" : \"REGISTER\",\"clientId\" : \"TJX\",\"referCode\" : \"\",\"comment\" : \"\",\"secondaryGroup\" : \"000\"},\"primaryApplicant\" : {\"tempPassDays\" : \"\",\"longitude\" : \"\",\"cardIndSEDS\" : \"\",\"promoTrack\" : \"\",\"employerState\" : \"\",\"spCode\" : \"\",\"bankCardNumber\" : \"\",\"signatureIndicator\" : \"\",\"customerLoanTerm\" : \"\",\"passportIssuingCountry\" : \"\",\"swipeEdit\" : \"\",\"address\" : {\"address2\" : \"\",\"state\" : \"IL\",\"address1\" : \"21 KING ARTHUR CT\",\"city\" : \"FANTASY ISLAND\",\"zipCode\" : \"60750\"},\"plateCode\" : \"\",\"ssn\" : \"666010080\",\"permanentResidentCard\" : \"\",\"passportNumber\" : \"\",\"cardIndVISA\" : \"\",\"cardIndAMEX\" : \"\",\"mobileStoreDistance\" : \"\",\"bankCardType\" : \"\",\"employerName\" : \"\",\"redemptionAmount\" : \"\",\"timeAtHome\" : \"\",\"latitude\" : \"\",\"applicationCountry\" : \"\",\"homePhone\" : \"1234567891\",\"requestedlineOfCredit\" : %@,\"relativeInfo\" : \"\",\"memberNumber\" : \"\",\"employerPhone\" : \"\",\"imageDocID\" : \"\",\"secLastName\" : \"\",\"optOutFlag\" : \"\",\"eBillEnrollment\" : \"\",\"cardIndMC\" : \"\",\"birthDate\" : \"19700605\",\"promoCode\" : \"\",\"incomeAnnual\" : \"12000000\",\"companyPhone\" : \"\",\"maidenName\" : \"\",\"authCosCode\" : \"\",\"insuranceCode\" : \"\",\"onlineLetter\" : \"\",\"billingLastName\" : \"\",\"caCivicNumber\" : \"\",\"previousAddress\" : {\"address2prvad\" : \"\",\"stateprvad\" : \"\",\"address1prvad\" : \"\",\"cityprvad\" : \"\",\"zipCodeprvad\" : \"\"},\"mobileDeviceType\" : \"\",\"lastName\" : \"APPROVAL\",\"billinngCycle\" : \"\",\"firstName\" : \"JOE\",\"ipAddress\" : \"\",\"purchaseAmount\" : \"\",\"extensionPhoneNumber\" : \"\",\"driverLicenseState\" : \"\",\"sourceCode\" : \"\",\"mothersBirthDate\" : \"\",\"driverLicenseExpiryDate\" : \"\",\"dlSsoId\" : \"\",\"memberSinceDate\" : \"\",\"poem\" : \"t@t.com\",\"reissueDate\" : \"\",\"modelIndicator\" : \"\",\"billingFirstName\" : \"\",\"residenceCode\" : \"\",\"timeAtJob\" : \"\",\"vehicleOwn\" : \"\",\"relativePhone\" : \"\",\"creditLineRqd\" : \"\",\"contractNumber\" : \"\",\"passportBirthCity\" : \"\",\"employerZip\" : \"\",\"cardDeptStrInd\" : \"\",\"blackBoxId\" : \"\",\"title\" : \"\",\"bankAccountCode\" : \"\",\"regionalBusinessCode\" : \"\",\"occupationCode\" : \"\",\"employerAddress\" : \"\",\"driverLicenseNumber\" : \"\",\"applicationType\" : \"\",\"accoutInfo\" : \"\",\"mortgageOrRentPay\" : \"\",\"extensionOverride\" : \"\",\"employerCity\" : \"\",\"middleName\" : \"\",\"suffix\" : \"\",\"ssnOverride\" : \"\",\"currentEmployer\" : \"\",\"relativeCode\" : \"\",\"routingNumber\" : \"\",\"cellPhone\" : \"\",\"frnLangIndicator\" : \"E\",\"checkingAccountNumber\" : \"\",\"memberType\" : \"\"}}}}", firstName, lastName, whateverthelastthingwas];
+    NSString *str = [NSString stringWithFormat:@"{\"apply\" : {\"applyRequest\" : {\"authCosApplicantData\" : {\"lastNameapp\" : \"\",\"firstNameapp\" : \"\",\"middleInitialapp\" : \"\",\"suffixapp\" : \"\",\"ssn1\" : \"\",\"birthDateapp\" : \"\",\"driverLicenseNumberapp\" : \"\",\"driverLicenseStateapp\" : \"\",\"signatureIndicatorapp\" : \"\", \"titleapp\" : \"\", \"occupationCodeapp\" : \"\",\"incomeAnnualapp\" : \"\",\"address\" : {\"address2app\" : \"\", \"stateapp\" : \"\",\"address1app\" : \"\",\"cityapp\" : \"\",\"zipCodeapp\" : \"\"},\"membershipNumberapp\" : \"\",\"ssnOverrideapp\" : \"\",\"driverLicenseExpiryDateapp\" : \"\"},\"spouseDetails\" : {\"spFirstName\" : \"\",\"spMiddleName\" : \"\",\"spLastName\" : \"\",\"address\" : {\"address2spd\" : \"\",\"statespd\" : \"\",\"address1spd\" : \"\",\"cityspd\" : \"\",\"zipCodespd\" : \"\"}},\"merchantData\" : {\"clientDepartment\" : \"\",\"iovationRequestType\" : \"application\",\"operator\" : \"ECOM\",\"clientData\" : \"\",\"merchantNumber\" : \"0000000000010032\",\"country\" : \"US\",\"deviceType\" : \"T\",\"originalProductCode\" : \"010\",\"rewardsCode\" : \"\",\"membershipNumber\" : \"\",\"primaryGroup\" : \"000\",\"empAccountCode\" : \"Y\",\"salesPerson\" : \"213006748\",\"clientDataShare\" : \"\",\"sendEmail\" : \"\",\"referalCode\" : \"\",\"register\" : \"REGISTER\",\"clientId\" : \"TJX\",\"referCode\" : \"\",\"comment\" : \"\",\"secondaryGroup\" : \"000\"},\"primaryApplicant\" : {\"tempPassDays\" : \"\",\"longitude\" : \"\",\"cardIndSEDS\" : \"\",\"promoTrack\" : \"\",\"employerState\" : \"\",\"spCode\" : \"\",\"bankCardNumber\" : \"\",\"signatureIndicator\" : \"\",\"customerLoanTerm\" : \"\",\"passportIssuingCountry\" : \"\",\"swipeEdit\" : \"\",\"address\" : {\"address2\" : \"\",\"state\" : \"IL\",\"address1\" : \"21 KING ARTHUR CT\",\"city\" : \"FANTASY ISLAND\",\"zipCode\" : \"60750\"},\"plateCode\" : \"\",\"ssn\" : \"666010080\",\"permanentResidentCard\" : \"\",\"passportNumber\" : \"\",\"cardIndVISA\" : \"\",\"cardIndAMEX\" : \"\",\"mobileStoreDistance\" : \"\",\"bankCardType\" : \"\",\"employerName\" : \"\",\"redemptionAmount\" : \"\",\"timeAtHome\" : \"\",\"latitude\" : \"\",\"applicationCountry\" : \"\",\"homePhone\" : \"1234567891\",\"requestedlineOfCredit\" : %@,\"relativeInfo\" : \"\",\"memberNumber\" : \"\",\"employerPhone\" : \"\",\"imageDocID\" : \"\",\"secLastName\" : \"\",\"optOutFlag\" : \"\",\"eBillEnrollment\" : \"\",\"cardIndMC\" : \"\",\"birthDate\" : \"19700605\",\"promoCode\" : \"\",\"incomeAnnual\" : \"12000000\",\"companyPhone\" : \"\",\"maidenName\" : \"\",\"authCosCode\" : \"\",\"insuranceCode\" : \"\",\"onlineLetter\" : \"\",\"billingLastName\" : \"\",\"caCivicNumber\" : \"\",\"previousAddress\" : {\"address2prvad\" : \"\",\"stateprvad\" : \"\",\"address1prvad\" : \"\",\"cityprvad\" : \"\",\"zipCodeprvad\" : \"\"},\"mobileDeviceType\" : \"\",\"lastName\" : \"APPROVAL\",\"billinngCycle\" : \"\",\"firstName\" : \"JOE\",\"ipAddress\" : \"\",\"purchaseAmount\" : \"\",\"extensionPhoneNumber\" : \"\",\"driverLicenseState\" : \"\",\"sourceCode\" : \"\",\"mothersBirthDate\" : \"\",\"driverLicenseExpiryDate\" : \"\",\"dlSsoId\" : \"\",\"memberSinceDate\" : \"\",\"email\" : \"t@t.com\",\"reissueDate\" : \"\",\"modelIndicator\" : \"\",\"billingFirstName\" : \"\",\"residenceCode\" : \"\",\"timeAtJob\" : \"\",\"vehicleOwn\" : \"\",\"relativePhone\" : \"\",\"creditLineRqd\" : \"\",\"contractNumber\" : \"\",\"passportBirthCity\" : \"\",\"employerZip\" : \"\",\"cardDeptStrInd\" : \"\",\"blackBoxId\" : \"\",\"title\" : \"\",\"bankAccountCode\" : \"\",\"regionalBusinessCode\" : \"\",\"occupationCode\" : \"\",\"employerAddress\" : \"\",\"driverLicenseNumber\" : \"\",\"applicationType\" : \"\",\"accoutInfo\" : \"\",\"mortgageOrRentPay\" : \"\",\"extensionOverride\" : \"\",\"employerCity\" : \"\",\"middleName\" : \"\",\"suffix\" : \"\",\"ssnOverride\" : \"\",\"currentEmployer\" : \"\",\"relativeCode\" : \"\",\"routingNumber\" : \"\",\"cellPhone\" : \"\",\"frnLangIndicator\" : \"E\",\"checkingAccountNumber\" : \"\",\"memberType\" : \"\"}}}}", whateverthelastthingwas];
 
     NSData *data = [str dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
 
@@ -110,20 +110,7 @@
     NSString *myString = [[NSString alloc] initWithData:connectionData encoding:NSUTF8StringEncoding];
 
     NSLog(@"%@",myString);
-//    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:connectionData options:kNilOptions error:&error];
 
-//    NSArray *items = [json valueForKeyPath:@"redeemable_certificates"];
-//
-//    NSInteger *credit = 0;
-//
-//    for(int i = 0; i< items.count; i++){
-//        //        NSDictionary *sketch = [items objectAtIndex:i];
-//        //        NSLog(@"credits is %d", (int)credit);
-//        //        NSLog(@"certificate dollar is %d", (NSInteger)[[sketch objectForKey:@"certificate_dollar_amount"] integerValue]);
-//        //        credit += (NSInteger)[[sketch objectForKey:@"certificate_dollar_amount"] integerValue];
-//        //        NSLog(@"final credit is %d", credit);
-//    }
-//    credit = items.count * 10;
     return true;
 }
 
@@ -171,6 +158,7 @@
     KLCPopup* popup = [KLCPopup popupWithContentView:contentView];
     [popup show];
 
+    [self getCredit];
 }
 
 /*
